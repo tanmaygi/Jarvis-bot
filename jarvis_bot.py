76bf53517651b61f65bcddde20ff66dc35fbf0f7
Tanmay <happytanmay2@gmail.com>
2021-09-23 19:54:29 +0530

    project complete

--- jarvis_bot.py
+++ jarvis_bot.py
@@ -1,43 +1,65 @@
-# jarvis bot start
-print("Hello! My name is Jarvis.")
-print("I was created in 2021.")
-print("Please, remind me your name.")
-your_name = input()
-# String Concatenation
-print("What a great name you have, " + your_name + "!")
-print("Let me guess your age.")
-print("Enter remainders of dividing your age by 3, 5 and 7.")
+def greet(bot_name, birth_year):
+    print('Hello! My name is ' + bot_name + '.')
+    print('I was created in ' + birth_year + '.')
 
-# Get the input from user
-rem3 = int(input("Remainder by 3:"))
-rem5 = int(input("Remainder by 5:"))
-rem7 = int(input("Remainder by 7:"))
 
-# Based on simple math trick
-# It turns out that for each number ranging from 0 to 104 the calculation will result in the number itself. 
-# This perfectly fits the ordinary age range, doesn't it!
-user_age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
-print("Your age is " + str(user_age) + "; that's a good time to start programming!")
+def remind_name():
+    print('Please, remind me your name.')
+    name = input()
+    print('What a great name you have, ' + name + '!')
 
-# counter – a variable that changes its value after each iteration.
-print("Now I will prove to you that I can count to any number you want.")
-num = int(input())
-counter = 0
-while counter <= num:
-    print(str(counter) + " !")
-    counter += 1 
-print("Completed, have a nice day") 
 
- # MCQ test
-print("Let's test your programming knowledge.")
+def guess_age():
+    print('Let me guess your age.')
+    print('Enter remainders of dividing your age by 3, 5 and 7.')
 
-print("Why do we use methods?")
-optiona = print("To repeat a statement multiple times.")
-optionb = print("To decompose a program into several small subroutines.")
-optionc = print("To determine the execution time of a program.")
-optiond = print("To interrupt the execution of a program.")
-  
-option = [optiona, optionb, optionc, optiond]
-# ans = int(input())
-# if ans == 2:
-#     print()
+    rem3 = int(input())
+    rem5 = int(input())
+    rem7 = int(input())
+    age = (rem3 * 70 + rem5 * 21 + rem7 * 15) % 105
+
+    print("Your age is " + str(age) + "; that's a good time to start programming!")
+
+
+def count():
+    print('Now I will prove to you that I can count to any number you want.')
+
+    num = int(input())
+    curr = 0
+    while curr <= num:
+        print(curr, '!')
+        curr = curr + 1
+
+
+def test():
+    print("Let's test your programming knowledge.")
+    # write your code here
+    print("What do we use to define a block of code in Python language?")
+    options = []
+    option_1 = "1. Key"
+    option_2 = "2. Brackets"
+    option_3 = "3. Indentations"
+    option_4 = "4. None"
+    print("option_1")
+    print("option_2")
+    print("option_3")
+    print("option_4")
+    answer = input()
+    if answer == "option_3":
+        print("Completed, have a nice day!")
+    else:
+        print("Please, try again.")
+    
+    print('Completed, have a nice day!')
+
+
+def end():
+    print('Congratulations, have a nice day!')
+
+
+greet('Aid', '2020')  # change it as you need
+remind_name()
+guess_age()
+count()
+test()
+end()
